9cd633cee2dda299a9ce047ec7cb1bd4f8830877
BrunoMaiaPinto <brunommpinto@sapo.pt>
2025-06-04 16:50:56 +0100

    Fix alert message condition in login.js to ensure proper display

--- public/js/login.js
+++ public/js/login.js
@@ -59,4 +59,4 @@ const logout = async () => {
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alert) showAlert('success', alertMessage, 20);
+if (alertMessage) showAlert('success', alertMessage, 20);
